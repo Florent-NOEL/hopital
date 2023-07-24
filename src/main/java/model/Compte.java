36fc0e9f138e79766fb005d89b8ddf333905f5c7
leoP002 <leopoldmn72@gmail.com>
2023-07-24 10:28:25 +0200

    ajout classe Compte

---
+++ src/main/java/model/Compte.java
@@ -0,0 +1,83 @@
+package model;
+
+import java.util.Objects;
+
+public abstract class Compte {
+	private Integer id;
+	private String login;
+	private String password;
+	
+	
+	
+	public Compte() {
+
+	}
+
+
+
+	public Compte(String login, String password) {
+		super();
+		this.login = login;
+		this.password = password;
+	}
+
+
+
+	public Integer getId() {
+		return id;
+	}
+
+
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+
+
+	public String getLogin() {
+		return login;
+	}
+
+
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Compte other = (Compte) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+
+}

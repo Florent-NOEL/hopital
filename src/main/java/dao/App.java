4371d4e0e44ea62f07789ebe2abdeb4aebfdfe9c
Florent-NOEL <121178866+Florent-NOEL@users.noreply.github.com>
2023-07-24 16:47:54 +0200

    start app

--- src/main/java/dao/App.java
+++ src/main/java/dao/App.java
@@ -4,6 +4,7 @@ import model.Compte;
 
 import java.time.LocalDate;
 import java.time.Month;
+import java.util.Scanner;
 
 import dao.DaoPatient;
 import model.Patient;
@@ -12,15 +13,47 @@ import model.Patient;
 public class App {
 	
 	public static void main(String[] args) {
-		  DaoCompte daoCompte = JdbcContext.getDaoCompte();
-	        System.out.println(daoCompte.findAll());
-	        
-		DaoPatient daoPatient=JdbcContext.getDaoPatient();
-		
-		
-		
+		secretaire();
+	
+	}
+
+	public static void secretaire(){
+		DaoPatient daoPatient = JdbcContext.getDaoPatient();
+
+		String nom = saisieString("nom du patient");
+		String prenom = saisieString("prenom");
+		System.out.println(nom+prenom);
 
 	}
 
+	public static void medecin(){
+		
+	}
+
+	static void saySomething(String msg) {
+		System.out.println(msg);
+	}
+
+	static int addition(int nombre1, int nombre2) {
+		return nombre1 + nombre2;
+	}
+
+	static int saisieInt(String message) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println(message);
+		return sc.nextInt();
+	}
+
+	static double saisieDouble(String message) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println(message);
+		return sc.nextDouble();
+	}
+
+	static String saisieString(String message) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println(message);
+		return sc.nextLine();
+	}
 
 }

27e89e47ae96adf38ad69a7f4e075a4bfd703c2d
Florent-NOEL <121178866+Florent-NOEL@users.noreply.github.com>
2023-07-28 09:22:27 +0200

    del import

--- src/main/java/model/Secretaire.java
+++ src/main/java/model/Secretaire.java
@@ -1,12 +1,7 @@
 package model;
 
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.ObjectOutputStream;
 import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.List;
-
 import dao.DaoPatient;
 import dao.JdbcContext;
 
